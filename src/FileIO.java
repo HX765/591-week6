ca91f6e802f1d80d26d2cf6b09d72ffc3b576150
Han Xiao <hannyc@seas.upenn.edu>
2019-07-01 11:57:01 -0400

    first commit

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class FileIO {
 
@@ -8,7 +12,36 @@ public class FileIO {
 		String operation = in.nextLine();
 		
 		if(operation.equalsIgnoreCase("w")) {
-			System.out.println("We're going to write a file!");
+			System.out.println("We're going to write a file! What should it be called?");
+			String filename = in.nextLine();
+			try {
+				FileWriter fw = new FileWriter(filename);
+				System.out.println("What text should we put in the file?");
+				String fileContents = in.nextLine();
+				fw.write(fileContents);
+				fw.close();
+				System.out.println("file was successfully written!");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		else if(operation.equalsIgnoreCase("r")) {
+			System.out.println("We're going to read a file! What is the file called?");
+			String filename = in.nextLine();
+			File f = new File(filename);
+			
+			try {
+				Scanner fileParser = new Scanner(f);
+				while(fileParser.hasNextLine()) {
+					System.out.println(fileParser.nextLine());
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 		
 		in.close();
